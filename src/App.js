583e9e644852e80c108b46fb16b9ae95871bad1b
Habibur Rahman <107278090+Habibur1212@users.noreply.github.com>
2023-08-22 17:32:20 +0530

    Basic Template Initiated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import useFetch from './useFetch';
 
 function App() {
+
+  const { error, isPending, data: data } = useFetch('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
